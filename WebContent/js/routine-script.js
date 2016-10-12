d415d51bcac34fa52bb086db24eb0721eb6473f2
Saiful Islam <sifzone@yahoo.com>
2016-10-12 02:37:44 +0200

    New Project created!

---
+++ WebContent/js/routine-script.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+var myapp = angular.module('rountine', []);
+
+myapp.controller('rtnCtrl', function() {
+	
+	var routineRow = {
+		code : "",
+		name : "",
+		credithr : "",
+		day : "",
+		time : "",
+		place : "",
+		startDate : ""
+	};
+	
+	var routineTable = {
+			
+	};
+	
+})
